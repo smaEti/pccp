7cc010fcc96597eaf1f43bf4fc0b1900318534c7
Amin Ehterami <A.Ehterami@proton.me>
2023-08-07 14:15:57 +0330

    loading component completed

--- app/loading.js
+++ app/loading.js
@@ -1,4 +1,17 @@
-import React from "react"
+import Image from "next/image";
+import logo from "@/public/logo.png";
+
 export default function Loading() {
-    return (<h1>Loading</h1>)
+    return (
+        <div className="h-screen w-screen flex justify-center items-center animate-bounce gap-5 ltr">
+            <h1 className="text-orange-600 text-2xl font-semibold">Loading</h1>
+            <Image
+          priority={true}
+            src={logo}
+            width={35}
+            height={35}
+            alt="شرکت پترو پالایش کاسپین شیمی پاسارگاد"
+          />
+        </div>
+    )
 }
